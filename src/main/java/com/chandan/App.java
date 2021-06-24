278b2e5a9766af2889065aea5303024eece2f64a
c_soni <c_soni@apple.com>
2021-06-24 13:05:47 +0530

    Initial Commit

---
+++ src/main/java/com/chandan/App.java
@@ -0,0 +1,13 @@
+package com.chandan;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
